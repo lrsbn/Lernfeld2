2ce18d473673b1f57dd50fd5a45e370292ebc48d
lars <lars.barnekow@gmx.de>
2021-06-10 10:57:29 +0200

    CSS and Small Changes to package.json

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import { HashRouter, NavLink } from 'react-router-dom';
+import React from "react";
+import { HashRouter, NavLink } from "react-router-dom";
 
 export const Header = () => {
-    return(
-        <div className="header">
-            <HashRouter>
-            <NavLink className="navbar-brand" to="/">Home</NavLink>
-            <NavLink className="navbar-brand" to="/calc">Calculator</NavLink> 
-            <NavLink className="navbar-brand" to="/Fahrkosten">Fahrkosten</NavLink> 
-            <NavLink className="navbar-brand" to="/fahrkosten_calc">Fahrkosten 2</NavLink>
-            </HashRouter>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="header-wrapper">
+        <HashRouter>
+          <NavLink className="navbar-brand" to="/">
+            WECONNECT
+          </NavLink>
+          <NavLink className="navbar-link" to="/calc">
+            Calculator
+          </NavLink>
+          <NavLink className="navbar-link" to="/Fahrkosten">
+            Fahrkosten
+          </NavLink>
+          <NavLink className="navbar-link" to="/fahrkosten_calc">
+            Fahrkosten 2
+          </NavLink>
+        </HashRouter>
+      </div>
+    </div>
+  );
+};
